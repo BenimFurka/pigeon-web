39142ba029ee8c7491d8219818705a4073e99836
furka <furkanchik00@mail.ru>
2025-05-02 15:24:20 +0400

    Mobile Support

---
+++ assets/app/js/height.js
@@ -0,0 +1,8 @@
+
+function updateBodyHeight() {
+    const doc = document.documentElement;
+    doc.style.setProperty('--window-height', `${window.innerHeight}px`);
+}
+
+window.addEventListener('resize', updateBodyHeight);
+updateBodyHeight();
